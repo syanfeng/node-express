b089ae3c3322b9fd051662ad9cf9b6b250fd844f
syanfeng <15021815774@163.com>
2018-01-26 19:33:39 +0800

    ddd

--- app/user.js
+++ app/user.js
@@ -81,58 +81,58 @@ router.get('/deleteUser/:id', function (req, res) {
 
 module.exports = router
 
-function user(app) {
-    app.get('/user/listUsers', function (req, res) {
-        console.log('__dirname:'+__dirname);
-        fs.readFile( __dirname + "/json/users.json", 'utf8', function (err, data) {
-            console.log('listUsers:', data );
-            res.end( data );
-        });
-    })
+// function user(app) {
+//     app.get('/user/listUsers', function (req, res) {
+//         console.log('__dirname:'+__dirname);
+//         fs.readFile( __dirname + "/json/users.json", 'utf8', function (err, data) {
+//             console.log('listUsers:', data );
+//             res.end( data );
+//         });
+//     })
     
 
-    app.get('/user/addUser', function (req, res) {
-        // 读取已存在的数据
-        fs.readFile( __dirname + "/json/" + "users.json", 'utf8', function (err, data) {
-            data = JSON.parse( data );
-            data["user4"] = user["user4"];
-            console.log( data );
-            fs.writeFile(__dirname + '/json/users.json', JSON.stringify(data),  function(err) {
-                if (err) {
-                    return console.error(err);
-                }
-                console.log("数据写入成功！");
-                console.log("--------我是分割线-------------")
-                console.log("读取写入的数据！");
-                fs.readFile(__dirname + "/json/" + "users.json", 'utf8', function (err, data) {
-                    if (err) {
-                        return console.error(err);
-                    }
-                    console.log("异步读取文件数据: " + data.toString());
-                    res.end( JSON.stringify(data));
-                });
-            });
-        });
-    })
+//     app.get('/user/addUser', function (req, res) {
+//         // 读取已存在的数据
+//         fs.readFile( __dirname + "/json/" + "users.json", 'utf8', function (err, data) {
+//             data = JSON.parse( data );
+//             data["user4"] = user["user4"];
+//             console.log( data );
+//             fs.writeFile(__dirname + '/json/users.json', JSON.stringify(data),  function(err) {
+//                 if (err) {
+//                     return console.error(err);
+//                 }
+//                 console.log("数据写入成功！");
+//                 console.log("--------我是分割线-------------")
+//                 console.log("读取写入的数据！");
+//                 fs.readFile(__dirname + "/json/" + "users.json", 'utf8', function (err, data) {
+//                     if (err) {
+//                         return console.error(err);
+//                     }
+//                     console.log("异步读取文件数据: " + data.toString());
+//                     res.end( JSON.stringify(data));
+//                 });
+//             });
+//         });
+//     })
 
-    app.get('/user/showUser/:id', function (req, res) {
-        // 首先我们读取已存在的用户
-        fs.readFile( __dirname + "/json/" + "users.json", 'utf8', function (err, data) {
-            data = JSON.parse( data );
-            var user = data["user" + req.params.id] 
-            console.log( user );
-            res.end( JSON.stringify(user));
-        });
-    })
+//     app.get('/user/showUser/:id', function (req, res) {
+//         // 首先我们读取已存在的用户
+//         fs.readFile( __dirname + "/json/" + "users.json", 'utf8', function (err, data) {
+//             data = JSON.parse( data );
+//             var user = data["user" + req.params.id] 
+//             console.log( user );
+//             res.end( JSON.stringify(user));
+//         });
+//     })
 
-    app.get('/user/deleteUser/:id', function (req, res) {
-        // First read existing users.
-        fs.readFile( __dirname + "/json/" + "users.json", 'utf8', function (err, data) {
-            data = JSON.parse( data );
-            delete data["user" + req.params.id];
+//     app.get('/user/deleteUser/:id', function (req, res) {
+//         // First read existing users.
+//         fs.readFile( __dirname + "/json/" + "users.json", 'utf8', function (err, data) {
+//             data = JSON.parse( data );
+//             delete data["user" + req.params.id];
             
-            console.log( data );
-            res.end( JSON.stringify(data));
-        });
-    })
-}
+//             console.log( data );
+//             res.end( JSON.stringify(data));
+//         });
+//     })
+// }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var fs = require("fs");
 var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser')
+var mysql = require('mysql');
 
 var user = require("./app/user");
 var birds = require('./app/birds')
@@ -13,8 +14,53 @@ app.use(cookieParser())
 // 创建 application/x-www-form-urlencoded 编码解析
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+//   database: 'world'
+// })
+
+// connection.connect(function(err) {
+//   if(err) throw err;
+//   console.log('connect success');
+// });
+
+// connection.query('SELECT * FROM `city`', function(err, result, fields) {
+//   if(err) throw err;
+//   console.log(result);
+// })
+// console.log('select ended');
+
+// connection.end(function(err) {
+//   if(err) throw err;
+//   console.log('connect end');
+// })
+
+
+var pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'test'
+});
+
+pool.getConnection(function (err, connection) {
+  if (err) throw err;
+  // connected! (unless `err` is set)
+  connection.query('SELECT * FROM `user`', function (error, results, fields) {
+    // And done with the connection.
+    console.log(results)
+
+    connection.release();
+
+    // Handle error after the release.
+    if (error) throw error;
+  });
+});
+
 // 静态文件处理
-app.use('/static',express.static(__dirname + '/static'));
+app.use('/static', express.static(__dirname + '/static'));
 // user 模块路由
 app.use('/user', user)
 // birds 模块路由
@@ -24,23 +70,23 @@ app.use('/account', account)
 
 // 首页 正则匹配index.html
 app.get('/?(index.html)?', function (req, res) {
-  res.sendFile( __dirname + "/" + "index.html" );
+  res.sendFile(__dirname + "/" + "index.html");
 })
 
 app.post('/process_post', urlencodedParser, function (req, res) {
   // 输出 JSON 格式
   response = {
-      first_name:req.body.first_name,
-      last_name:req.body.last_name
+    first_name: req.body.first_name,
+    last_name: req.body.last_name
   };
   console.log(response);
   res.end(JSON.stringify(response));
 })
 
 // 没有匹配的路由指向这里
-app.get('*', function(req, res){
+app.get('*', function (req, res) {
   console.log("*", res)
-  res.sendFile( __dirname + "/" + "404.html" );
+  res.sendFile(__dirname + "/" + "404.html");
 });
 
 var server = app.listen(8081, function () {
